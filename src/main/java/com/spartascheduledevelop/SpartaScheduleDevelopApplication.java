06994034d2da3dd5706da3649eb4dbda73b55daa
sangmu <dltkdandl@naver.com>
2025-11-19 16:06:41 +0900

    SpartaScheduleDevelop 과제 생성

---
+++ src/main/java/com/spartascheduledevelop/SpartaScheduleDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.spartascheduledevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaScheduleDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaScheduleDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spartascheduledevelop/SpartaScheduleDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartascheduledevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaScheduleDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
